dbbb74e265c2f86db8c831fdb4aa5a804c554d3d
chosakuken <shunpc2018@outlook.jp>
2026-05-13 15:25:33 +0900

    :sparkles: feat: Taskコントローラの追加 (#5)

--- backend/src/config/express.js
+++ backend/src/config/express.js
@@ -1,12 +1,14 @@
 "use strict";
 const express = require("express");
 const { rootRouter } = require("../controller/root");
+const { taskRouter } = require("../controller/task");
 
 // アプリの実体を定義する
 const setupExpress = () => {
   const app = express();
   // コントローラ定義
   app.use("/", rootRouter);
+  app.use("/task", taskRouter);
   return app;
 };
 
