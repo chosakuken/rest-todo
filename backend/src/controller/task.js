c791987c7ae6c3c3ac3596c5ea7f209288f2ffa8
chosakuken <shunpc2018@outlook.jp>
2026-05-13 17:44:33 +0900

    :sparkles: feat: 作成機能をコントローラに紐づけ (#9)

--- backend/src/controller/task.js
+++ backend/src/controller/task.js
@@ -1,9 +1,35 @@
 "use strict";
+
+const { createTask } = require("../service/task");
+
 const taskRouter = require("express").Router();
 
 // 仮実装
 taskRouter.get("/", (req, res) => {
   res.send("Taskテーブルからデータが取れる予定");
+});
+
+taskRouter.post("/", async (req, res) => {
+  // 今回は try-catch で握りつぶしているが
+  // 本来はこのタイミングでのバリデーションが必要
+  try {
+    const createdData = await createTask({
+      name: req.body.name,
+      detail: req.body.detail,
+      deadline: req.body.deadline,
+    });
+    return res.status(200).send(createdData.toJSON());
+  } catch (e) {
+    return res.status(400).send({
+      errors: [
+        {
+          status: 400,
+          type: "bad request",
+          detail: e,
+        },
+      ],
+    });
+  }
 });
 
 module.exports = { taskRouter };
